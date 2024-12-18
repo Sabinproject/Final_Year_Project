c95abb76b8c22f7608ff3a9d8a4c9a1f7be7f0f7
Sabinproject <166901333+Sabinproject@users.noreply.github.com>
2024-12-18 11:28:51 +0545

    Delete LostAndFound-Website directory

--- LostAndFound-Website/lost/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import itemlost
-#from .models import itemfound,RegistrationData
-from .models import itemfound
-
-# Register your models here.
-admin.site.register(itemlost)
-admin.site.register(itemfound)
-#admin.site.register(RegistrationData)
